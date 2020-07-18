0cc557b0a25f0fb3495b2dcd0c4aeb76d01ca10f
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-07-18 19:39:57 -0400

    Updating file - timer needs work.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // Pseudo Code
 
 // When the user clicks “Start Quiz” button
-	// Show “View High Scores” link in upper left hand corner
-	// Start countdown timer in upper right hand corner
-	// Show first question with multiple choice answers
+// Show “View High Scores” link in upper left hand corner
+// Start countdown timer in upper right hand corner
+// Show first question with multiple choice answers
 // When user selects correct answer (1 out of 4), return “Correct!”
 // When user selects wrong answer (3 out of 4), return “Wrong!”
-	// When user selects wrong answer, deduct 10 seconds from time
+// When user selects wrong answer, deduct 10 seconds from time
 // After returning answer notification, show next question
 // Repeat steps with the remaining questions
 // When all questions are answered or the timer reaches zero
-	// Return “All done!”
-	// Return “Your final score is:____”
-	// Return “Enter Initials: [text box] with [submit button]
+// Return “All done!”
+// Return “Your final score is:____”
+// Return “Enter Initials: [text box] with [submit button]
 // When user hits [submit button]
-	// Return “High Scores” with [initials] + [score]
-	// Buttons [go back] & [clear scores]
-		// When [clear scores] selected, clear scores from page
-        // When [go back] selected, return to main page (coding quiz challenge)
-        
+// Return “High Scores” with [initials] + [score]
+// Buttons [go back] & [clear scores]
+// When [clear scores] selected, clear scores from page
+// When [go back] selected, return to main page (coding quiz challenge)
+
 
 
 // Quiz Questions - JS or part of HTML?
@@ -59,24 +59,94 @@
 
 
 
+// Array of Questions
+// Source: https://stackoverflow.com/questions/40835372/targeting-multiple-choice-questions
+let questions = [
 
-// Countdown Timer
-var counter = 60;
-var interval = setInterval(function() {
-    counter--;
-    // Display 'counter' wherever you want to display it.
-    if (counter <= 0) {
-     	clearInterval(interval);
-      	$("#timer").html("Times up!");  
-        return;
-    } else {
-    	$("#time").text(counter);
-    }
-}, 1000);
+    {
+        question: "Commonly used data types DO NOT include:",
+        choice1: "Strings",
+        choice2: "Booleans",
+        choice3: "Alerts",
+        choice4: "Numbers",
+        correct: "3"
+    },
+
+    {
+        question: "The condition in an if / else statement is enclosed within ______.",
+        choice1: "Quotes",
+        choice2: "Curly Brackets",
+        choice3: "Parentheses",
+        choice4: "Square Brackets",
+        correct: "3"
+    },
+
+    {
+        question: "Arrays in JavaScript can be used to store ______.",
+        choice1: "Numbers & Strings",
+        choice2: "Other Arrays",
+        choice3: "Booleans",
+        choice4: "All of the above",
+        correct: "4"
+    },
+
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        choice1: "Commas",
+        choice2: "Curly Brackets",
+        choice3: "Quotes",
+        choice4: "Parentheses",
+        correct: "3"
+    },
+
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choice1: "JavaScript",
+        choice2: "Terminal / Bash",
+        choice3: "For Loops",
+        choice4: "Console Log",
+        correct: "4"
+    },
+
+];
+
+let start = document.querySelector("#start");
+let counter = 60;
+let timer;
+
+
+        // // Countdown Timer
+        // var counter = 60;
+        // var interval = setInterval(function () {
+        //     counter--;
+        //     if (counter <= 0) {
+        //         clearInterval(interval);
+        //         $("#timer").html("Times up!");
+        //         return;
+        //     } else {
+        //         $("#time").text(counter);
+        //     }
+        // }, 1000);
 
 
 function startQuiz() {
 
+    $("#start").on("click", function () {
+        // Countdown Timer
+        timer = setInterval(function () {
+            counter--;
+            $("#time").text(counter);
+            if (counter <= 0) {
+                clearInterval(timer);
+                $("#timer").html("Times up!");
+                return;
+            } 
+        }, 1000);
+        startQuiz();
+
+    }
 
 
-}
+
+
+    )}
